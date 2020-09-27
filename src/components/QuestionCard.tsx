6895f22d663ae6de1cca038f20f0e1ed0f5f844b
Nicky Lai <nicky.lai@lloydsbanking.com>
2020-09-27 16:14:52 +0100

    fourth commit

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 //type
 import { AnswerObject } from "../App";
+//styles
+import { Wrapper, ButtonWrapper } from "./QuestionCard.style";
 
 type Props = {
   question: string;
@@ -19,21 +21,25 @@ const QuestionCard: React.FC<Props> = ({
   questionNr,
   totalQuestions
 }) => (
-  <div>
+  <Wrapper>
     <p className="number">
       Question: {questionNr} / {totalQuestions}
     </p>
     <p dangerouslySetInnerHTML={{ __html: question }} />
     <div>
       {answers.map(answer => (
-        <div key={answer}>
+        <ButtonWrapper
+          key={answer}
+          correct={userAnswer?.correctAnswer === answer}
+          userClicked={userAnswer?.answer === answer}
+        >
           <button disabled={!!userAnswer} value={answer} onClick={callback}>
             <span dangerouslySetInnerHTML={{ __html: answer }} />
           </button>
-        </div>
+        </ButtonWrapper>
       ))}
     </div>
-  </div>
+  </Wrapper>
 );
 
 export default QuestionCard;
